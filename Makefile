479f2af4f5a8125d72a7f7742ac20684e9ef6f58
Jason Reed <jcreed@gmail.com>
2025-12-12 14:16:58 -0500

    Generate PDF

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 all:
 	pandoc -f markdown+autolink_bare_uris -sN  workbench-doc.md --output workbench-doc.html --css doc.css --metadata title="Lean Workbench" --lua-filter=mermaid.lua
+
+pdf:
+	pandoc -f markdown+autolink_bare_uris -sN  workbench-doc.md --output workbench-doc.pdf --metadata title="Lean Workbench" --lua-filter=mermaid-png.lua -V colorlinks=true -V urlcolor=blue
---
+++ mermaid-png.lua
@@ -0,0 +1,24 @@
+local diagram_count = 0
+
+function CodeBlock(el)
+  if el.classes:includes("mermaid") then
+    local tmp = os.tmpname() .. ".mmd"
+    diagram_count = diagram_count + 1
+    local out = "diagram_" .. diagram_count .. ".png"
+    -- write Mermaid code to tmp file
+    print(tmp)
+    local f = io.open(tmp, "w")
+    f:write(el.text)
+    f:close()
+    -- call mermaid-cli
+    os.execute("mmdc -s 3 -i " .. tmp .. " -o " .. out)
+
+    -- Create inline image
+    local img = pandoc.Image("diagram", out)
+
+    -- Wrap image in a link to the SVG file
+    local link = pandoc.Link(img, out)
+
+    return pandoc.Para{ link }
+  end
+end
